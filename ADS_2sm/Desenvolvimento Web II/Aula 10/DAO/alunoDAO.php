a9ab9f9a7de099b0372a4b1f99c8a46f4d9aef86
M. César Brancalhão <58019405+cesarbrancalhao@users.noreply.github.com>
2023-09-27 21:41:05 -0300

    Update alunoDAO.php

--- ADS_2sm/Desenvolvimento Web II/Aula 10/DAO/alunoDAO.php
+++ ADS_2sm/Desenvolvimento Web II/Aula 10/DAO/alunoDAO.php
@@ -7,10 +7,24 @@ class alunoDAO{
     public function list(){
         $conn = Connection::getConnection();
 
-        $sql = "SELECT * FROM  aluno";
+        $sql = "SELECT a.*, c.nome AS nome_curso FROM  alunos a" .
+               " JOIN cursos c ON (c.id = a.id_curso)";
         $stm = $conn->prepare($sql);
         $stm->execute();
         $result = $stm->fetchAll();
         return $result;
+    }
+
+    private function mapDBToObject(array $result){
+        $alunos = array();
+        foreach ($result as $reg) {
+            $aluno = new Aluno();
+            $aluno->setId($reg['id']);
+            $aluno->setNome($reg['nome']);
+            $aluno->setIdade($reg['idade']);
+            $aluno->setEstrangeiro($reg['estrangeiro']);
+
+            $curso = new Curso();
+        }
     }
 }
