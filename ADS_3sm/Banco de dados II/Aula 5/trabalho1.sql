f8c65691f112b0ab9ca5aad66a61f6f0a934eb4e
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-03-14 06:31:22 -0300

    Trabalho 1

--- ADS_3sm/Banco de dados II/Aula 5/trabalho1.sql
+++ ADS_3sm/Banco de dados II/Aula 5/trabalho1.sql
@@ -10,23 +10,130 @@ Faça consultas para os problemas abaixo:
 5. Apresente categoria que gerou maior renda
 */
 
-/* 1 */
+/* 1.  Apresente a lista dos 10 clientes que mais gastaram em locações. */
 SELECT 
-	initcap(first_name || ' ' || last_name) as full_name,
+	initcap(first_name || ' ' || lASt_name) AS full_name,
 	amount
 FROM payment
 INNER JOIN customer USING(customer_id)
 ORDER BY amount DESC
 LIMIT 10;
 
-/* 2 */
-SELECT 
+/* 2. Descubra qual cidade possui: */
+-- a. Mais clientes na locadora.
+SELECT
 	city,
-	COUNT(customer.customer_id) as customers_num,
-	COUNT(rental.rental_id) as rental_num
+	COUNT(customer.customer_id) AS customers_num,
+	SUM(rental_count) AS rental_num,
+	SUM(rental_amount) AS rental_value
 FROM city
 INNER JOIN address ON address.city_id = city.city_id
 INNER JOIN customer USING(address_id)
-INNER JOIN rental USING(customer_id)
+LEFT JOIN (
+	SELECT
+		customer_id,
+		COUNT(rental_id) AS rental_count
+	FROM rental
+	GROUP BY customer_id
+) AS rentals ON rentals.customer_id = customer.customer_id
+LEFT JOIN (
+	SELECT
+		customer_id,
+		SUM(amount) AS rental_amount
+	FROM payment
+	GROUP BY customer_id
+) AS payments ON payments.customer_id = customer.customer_id
 WHERE city.city_id = address.city_id
-GROUP BY city.city_id ORDER BY customers_num DESC;
+GROUP BY city.city_id ORDER BY customers_num DESC, rental_num DESC, rental_value DESC
+LIMIT 20;
+
+-- b Maior número de locações.
+SELECT
+	city,
+	COUNT(customer.customer_id) AS customers_num,
+	SUM(rental_count) AS rental_num,
+	SUM(rental_amount) AS rental_value
+FROM city
+INNER JOIN address ON address.city_id = city.city_id
+INNER JOIN customer USING(address_id)
+LEFT JOIN (
+	SELECT
+		customer_id,
+		COUNT(rental_id) AS rental_count
+	FROM rental
+	GROUP BY customer_id
+) AS rentals ON rentals.customer_id = customer.customer_id
+LEFT JOIN (
+	SELECT
+		customer_id,
+		SUM(amount) AS rental_amount
+	FROM payment
+	GROUP BY customer_id
+) AS payments ON payments.customer_id = customer.customer_id
+WHERE city.city_id = address.city_id
+GROUP BY city.city_id ORDER BY rental_num DESC, customers_num DESC, rental_value DESC
+LIMIT 20;
+
+-- c Maior valor de locações.
+SELECT
+	city,
+	COUNT(customer.customer_id) AS customers_num,
+	SUM(rental_count) AS rental_num,
+	SUM(rental_amount) AS rental_value
+FROM city
+INNER JOIN address ON address.city_id = city.city_id
+INNER JOIN customer USING(address_id)
+LEFT JOIN (
+	SELECT
+		customer_id,
+		COUNT(rental_id) AS rental_count
+	FROM rental
+	GROUP BY customer_id
+) AS rentals ON rentals.customer_id = customer.customer_id
+LEFT JOIN (
+	SELECT
+		customer_id,
+		SUM(amount) AS rental_amount
+	FROM payment
+	GROUP BY customer_id
+) AS payments ON payments.customer_id = customer.customer_id
+WHERE city.city_id = address.city_id
+GROUP BY city.city_id ORDER BY rental_value DESC, rental_num DESC, customers_num DESC
+LIMIT 20;
+
+/* 3. Qual loja possui mais filmes e maior número de exemplares. */
+SELECT
+	store.store_id,
+	COUNT(DISTINCT film_id) AS film_num,
+	SUM(inventory.inventory_id) AS inventory_num
+FROM store
+LEFT JOIN inventory ON inventory.store_id = store.store_id
+GROUP BY store.store_id
+ORDER BY film_num DESC, inventory_num DESC;
+
+/* 4. Descubra quantos filmes existem por categoria. */
+SELECT
+	name,
+	SUM(film_total) AS film_num
+FROM category
+LEFT JOIN (
+	SELECT
+		category_id,
+		COUNT(film_id) AS film_total
+	FROM film_category
+	GROUP BY category_id
+) AS films ON films.category_id = category.category_id
+GROUP BY category.name
+ORDER BY film_num DESC;
+
+/* 5. Apresente categoria que gerou maior renda */
+SELECT
+	cat.name,
+	SUM(payment.amount) as category_amount
+FROM payment
+JOIN rental AS ren ON ren.rental_id = payment.rental_id
+JOIN inventory AS inv ON inv.inventory_id = ren.inventory_id
+JOIN film_category AS fc ON fc.film_id = inv.film_id
+JOIN category AS cat ON cat.category_id = fc.category_id
+GROUP BY cat.name
+ORDER BY category_amount DESC;
