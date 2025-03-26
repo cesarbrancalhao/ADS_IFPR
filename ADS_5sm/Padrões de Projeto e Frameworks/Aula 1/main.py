a2016b0fe2525fd0851a6becbac2b41cfe47a102
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2025-03-26 11:13:20 -0300

    Aula 21/03

---
+++ ADS_5sm/Padrões de Projeto e Frameworks/Aula 1/main.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+class Pessoa(ABC):
+    def __init__(self, nome: str, renda: float):
+        self.nome = nome
+        self.renda = renda
+
+    @abstractmethod
+    def calculaIR(self):
+        return self.renda * 0.27
+
+class PessoaFisica(Pessoa):
+    def __init__(self, nome: str, renda: float, cpf: str):
+        super().__init__(nome, renda)
+        self.cpf = cpf
+    
+    def calculaIR(self):
+        return self.renda * 0.25
+
+class PessoaJuridica(Pessoa):
+    def __init__(self, nome: str, renda: float, cpf: str):
+        super().__init__(nome, renda)
+        self.cpf = cpf
+
+    def calculaIR(self):
+        return self.renda * 0.15
+
+pf = PessoaFisica("Maria", 100_000, "100.9999.000-99")
+print(pf.calculaIR())
